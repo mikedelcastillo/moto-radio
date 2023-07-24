d1a29cd98811eb05e8c4339e0c25b18315a0fd18
mikedelcastillo <johnmichaeldc@gmail.com>
2023-07-24 15:41:53 +0000

    fix: pressing switch channels on multiple controllers

--- arduino/TxPi/TxPi.ino
+++ arduino/TxPi/TxPi.ino
@@ -13,12 +13,11 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 typedef struct
 {
   uint8_t addressIndex;
+  bool wasPressingSwitchChannels;
 } Controller;
 
-Controller CONTROLLERS[MAX_CONTROLLERS] = {{0}, {1}, {2}, {3}};
+Controller CONTROLLERS[MAX_CONTROLLERS] = {{0, false}, {1, false}, {2, false}, {3, false}};
 ControllerInput CONTROLLER_INPUT;
-
-bool wasPressingSwitchChannels = false;
 
 uint8_t serialBuffer[SERIAL_BUFFER_LENGTH];
 uint8_t serialBufferLength = 0;
@@ -83,18 +82,19 @@ void processMessage()
     if (safeIndex)
     {
       controllerInputFromBuffer(serialBuffer, &CONTROLLER_INPUT);
+      Controller controller = CONTROLLERS[index];
+
       // Check if center button is pressed
       bool isPressingSwitchChannels = CONTROLLER_INPUT.BUTTON_CENTER == MAX_INT_RADIO_VALUE && CONTROLLER_INPUT.BUTTON_SELECT == MAX_INT_RADIO_VALUE;
-      if (isPressingSwitchChannels && !wasPressingSwitchChannels)
+      if (isPressingSwitchChannels && !controller.wasPressingSwitchChannels)
       {
         // Update controller channel
         int nextIndex = CONTROLLERS[index].addressIndex + 1;
         CONTROLLERS[index].addressIndex = nextIndex >= MAX_RADIO_ADDRESSES ? 0 : nextIndex;
       }
-      wasPressingSwitchChannels = isPressingSwitchChannels;
+      controller.wasPressingSwitchChannels = isPressingSwitchChannels;
 
       // Transmit data
-      Controller controller = CONTROLLERS[index];
       radio.stopListening();
       radio.openWritingPipe(RADIO_ADDRESSES[controller.addressIndex]);
       radio.writeFast(&CONTROLLER_INPUT, sizeof(ControllerInput));
